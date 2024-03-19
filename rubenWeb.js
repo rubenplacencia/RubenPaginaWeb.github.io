2ff60d9f32ac1394d333229167f5a48d38bf1bf9
rubenplacencia <ruben_placencia09@hotmail.com>
2024-03-19 16:36:21 -0600

    Se agrego un movimiento en la parte de frases

--- rubenWeb.js
+++ rubenWeb.js
@@ -8,8 +8,19 @@ const app = Vue.createApp({
                 'imagen4.jpg',
               ],
               cambio: 'imagen2,',
-            indiceImagen: 0
+            indiceImagen: 0,
+            frasesDiv: '',
+            frasesLibro: ['“La formación de hábitos es un proceso mediante el cual una conducta se vuelve progresivamente automática mediante la repetición constante“',
+             '“Si realmente quieres algo, puedes conseguirlo. Pero la verdad es que nuestra verdadera motivación consiste en ser perezosos y hacer lo que es más conveniente“',
+             '“Cada hábito que ponemos en manos de la tecnología libera tiempo y energía para que los pongamos al servicio de nuestra siguiente etapa de crecimiento',
+             '“Me he dado cuenta que con frecuencia imitó el comportamiento de aquellos que me rodean, sin darme cuenta“'],
+            i: 0,
+
         };
+    },
+    mounted(){
+        this.frasesDiv = document.getElementById('frasesLibros1');
+        setInterval(this.cambiarFrase, 5000)
     },
     methods: {
            
@@ -17,15 +28,19 @@ const app = Vue.createApp({
             this.abrirVentana = ventana
         },
 
-        cambioImagen(){
-            document.getElementById('imagen').src = 'Imagenes/' + this.imagenes[this.indiceImagen]
-            alert(this.imagenes[this.indiceImagen])
-            this.indiceImagen = this.indiceImagen + 1
-            if(this.indiceImagen >= this.imagenes.length){
-                this.indiceImagen = 0;
-            }
-        }
-    }
+
+        cambiarFrase(){
+          this.frasesDiv.innerText = this.frasesLibro[this.i];
+          this.i++;
+          if(this.i >= this.frasesLibro.length){
+             this.i = 0;
+          };
+        },
+        //INTERVALO DE FRASES 
+        
+        
+
+    },
     // //////////////////////////////////////////////////////////////////////////////fin de data
 });
 
